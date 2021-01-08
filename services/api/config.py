8dd72970e5e843aa4458e6a4fa9bcdaf7ce9109c
Raragyay <let987let987@gmail.com>
2021-01-07 19:45:09 -0500

    Missing file from previous commit added

--- services/api/config.py
+++ services/api/config.py
@@ -4,4 +4,5 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgres://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    POOL_PRE_PING = True
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_pre_ping': True}
