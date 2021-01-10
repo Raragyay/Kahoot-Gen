5b451ad679682267ccef2d4b5220f6daea6ddb18
Raragyay <let987let987@gmail.com>
2021-01-09 20:50:49 -0500

    Added purgecss

--- services/client/craco.config.js
+++ services/client/craco.config.js
@@ -1,6 +1,16 @@
 const CracoLessPlugin = require('craco-less');
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
+    style: {
+        postcss: {
+            plugins: [
+                purgecss({
+                    content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js', './src/**/*.ts', './src/**/*.tsx']
+                })
+            ]
+        }
+    },
     plugins: [
         {
             plugin: CracoLessPlugin,
